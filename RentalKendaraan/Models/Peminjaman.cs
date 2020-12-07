e67ae25663e761b4e8e701f79bf75cf5d44e9d5a
yusufhardi10 <yusufhardi10@gmail.com>
2020-12-08 01:15:10 +0700

    Add project files.

---
+++ RentalKendaraan/Models/Peminjaman.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RentalKendaraan_001.Models
+{
+    public partial class Peminjaman
+    {
+        public Peminjaman()
+        {
+            Pengembalian = new HashSet<Pengembalian>();
+        }
+
+        public int IdPeminjaman { get; set; }
+        public DateTime? TglPeminjaman { get; set; }
+        public int? IdKendaraan { get; set; }
+        public int? IdCustomer { get; set; }
+        public int? IdJaminan { get; set; }
+        public int? Biaya { get; set; }
+
+        public Jaminan BiayaNavigation { get; set; }
+        public Kendaraan IdCustomer1 { get; set; }
+        public Customer IdCustomerNavigation { get; set; }
+        public ICollection<Pengembalian> Pengembalian { get; set; }
+    }
+}
